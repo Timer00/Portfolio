53951117c3ae20f53c3f9d826d3f3a25a8ebece6
Theo <theocarraraleao@gmail.com>
2020-07-20 15:44:45 -0300

    Squares into projects

--- src/components/PROJ.js
+++ src/components/PROJ.js
@@ -4,16 +4,17 @@ class PROJ extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      animate: false
+      animate: false,
+      border: false
     };
   }
   open = () => {
-    this.setState({animate: true});
+    this.setState({animate: true,border: true});
     this.props.changeParentState('goBlue',!this.props.parentState.goBlue);
   };
 
   close = () => {
-    this.setState({animate: false});
+    this.setState({animate: false, border: false});
     this.props.changeParentState('goBlue',!this.props.parentState.goBlue);
   };
 
@@ -25,13 +26,21 @@ class PROJ extends Component {
     let titleEnd = 'titleEnd';
     titleEnd += this.state.animate ? ' titleFade' : '';
 
-    let contentClass = 'content marginTop';
+    let contentClass = 'content';
     contentClass += this.state.animate ? ' fadeIn' : '';
+
+    let subdivision = 'subdivision';
+    subdivision += this.state.border ? ' border' : '';
+    console.log(subdivision);
 
     return (
       <div onMouseEnter={this.open} onMouseLeave={this.close} id={this.props.id} className={divClass}>
         <div className={contentClass}>
-          <div className='projects'></div>
+          <div className={subdivision} style={{minWidth: "50%", minHeight: "40%"}}></div>
+          <div className={subdivision} style={{minWidth: "50%", minHeight: "40%"}}></div>
+          <div className={subdivision} style={{minWidth: "50%", minHeight: "33%"}}></div>
+          <div className={subdivision} style={{minWidth: "50%", minHeight: "33%"}}></div>
+          <div className={subdivision} style={{minWidth: "50%", minHeight: "33%"}}></div>
         </div>
         <div className={`title ${this.state.animate ? 'textLeft' : ''}`}>PROJ</div>
         <div className={titleEnd}>ECTS</div>
--- src/components/TECH.js
+++ src/components/TECH.js
@@ -28,20 +28,7 @@ class TECH extends Component {
     return (
       <div onMouseEnter={this.open} onMouseLeave={this.close} id={this.props.id} className={divClass}>
         <div className={contentClass}>
-          <div className='techPics'>
-            <h3>Javascript</h3>
-            <img src={img.javascript}/>
-          </div>
-          <div className='techPics'><h3>CSS</h3><img src={img.css}/></div>
-          <div className='techPics'><h3>HTML</h3><img src={img.html}/></div>
-          <div className='techPics'><h3>Django</h3><img src={img.django}/></div>
-          <div className='techPics'><h3>Python</h3><img src={img.python}/></div>
-          <div className='techPics'><h3>React</h3><img src={img.reactlogo}/></div>
-          <div className='techPics'><h3>MySql</h3><img src={img.mysql}/></div>
-          <div className='techPics'><h3>NodeJs</h3><img src={img.nodejs}/></div>
-          <div className='techPics'><h3>Unity</h3><img src={img.unity}/></div>
-          <div className='techPics'><h3>Photoshop</h3><img src={img.photoshop}/></div>
-          <div className='techPics'><h3>Illustrator</h3><img src={img.illustrator}/></div>
+
         </div>
         <div className={`title ${this.state.animate ? 'textLeft' : ''}`}>TECH</div>
       </div>
